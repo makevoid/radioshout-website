0d9814b832662607ba012a7652c679f5d5606b84
Francesco Canessa <makevoid@gmail.com>
2013-11-02 23:43:44 +0100

    update palinsesto

--- config/env.rb
+++ config/env.rb
@@ -28,3 +28,38 @@ end
 require_all "#{path}/models"
 # DataMapper.finalize
 
+## TODO: move in lib
+
+# Morpher
+
+# usage:
+#
+#   # view (haml)
+#   
+#   - palinsesto = File.open("views/palinsesto.textile", "r:UTF-8").read
+#   = morph_html RedCloth.new(palinsesto).to_html}
+#
+
+# rb
+
+module Morpher
+  def morph_html(html)
+    doc = Nokogiri::HTML html
+    doc.search("td").each do |td|
+      if td.inner_text.strip == "x"
+        td['class'] = "program"
+        td.content = ""
+      elsif td.inner_text =~ /®/
+        td['class'] = "replica"
+      elsif td.inner_text =~ /♪/ || td.inner_text =~ /:(.+)-(.+):/
+        # do nothing for now:  ♪
+      else
+        td['class'] = "program"
+      end
+    end
+    doc.to_html
+  end
+end
+
+include Morpher
+
